68023e0fb3aa190d6369aabdfa43b3c64951e470
karandeepkalra <122427044+karandeepkalra@users.noreply.github.com>
2024-09-05 00:09:07 +0530

    Delete c_project/tempCodeRunnerFile.c

--- c_project/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-if (deathf == 0 && print_flag == 0)
-        // {
-        //     update_ghosts2(&ghost1, ghost_count, &deathf, &print_flag, maze, player, items);
-        //     update_ghosts2(&ghost2, ghost_count, &deathf, &print_flag, maze, player, items);
-        //     update_ghosts2(&ghost3, ghost_count, &deathf, &print_flag, maze, player, items);
-        //     update_ghosts2(&ghost4, ghost_count, &deathf, &print_flag, maze, player, items);
-        //     update_screen(maze, SIZE_R, SIZE_C, items);
-        // }
